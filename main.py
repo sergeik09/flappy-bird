36a4f1da9f0bda30e51a127d56c9978c81baabf7
sergej <skoprov@icloud.com>
2025-05-21 18:19:21 +0300

    initial commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ TILE_SCALE = 3
 pg.init()
 
 font = pg.font.Font(None, 30)
+score_font = pg.font.Font(None, 70)
 
 FPS = 60
 
@@ -22,38 +23,38 @@ class Pipe(pg.sprite.Sprite):
         x = pipe_len % 4 * pipe_size_x
         y = pipe_len // 4 * pipe_size_y
         rect = pg.Rect(x, y, pipe_size_x, pipe_size_y)
-        self.image = spritesheet.subsurface(rect)
+        pipe_image = spritesheet.subsurface(rect)
         
         if is_top:
-            self.image = pg.transform.flip(self.image, False, True)
-
-        self.image = pg.transform.scale(self.image, (pipe_size_x * TILE_SCALE, height))
+            pipe_image = pg.transform.flip(pipe_image, False, True)
+            
+        pipe_image = pg.transform.scale(pipe_image, (pipe_size_x * TILE_SCALE, pipe_size_y * TILE_SCALE))
+        
+        num_pipes = height // (pipe_size_y * TILE_SCALE) + 1
+        self.image = pg.Surface((pipe_size_x * TILE_SCALE, height), pg.SRCALPHA)
+        
+        for i in range(num_pipes):
+            y_pos = i * (pipe_size_y * TILE_SCALE)
+            if is_top:
+                y_pos = height - (i + 1) * (pipe_size_y * TILE_SCALE)
+            self.image.blit(pipe_image, (0, y_pos))
+            
         self.rect = self.image.get_rect()
-
         self.rect.x = SCREEN_WIDTH
         
-        gap_size = 200  # Расстояние между трубами для пролета птицы
-        
         if is_top:
-            self.rect.top = 0  # Верхняя труба касается верхней границы
+            self.rect.top = 0
         else:
-            self.rect.bottom = SCREEN_HEIGHT  # Нижняя труба касается нижней границы
+            self.rect.bottom = SCREEN_HEIGHT
             
-
-
         self.timer = pg.time.get_ticks()
         self.speed_interval = 100
-
 
     def update(self, speed):
         self.rect.x -= speed
 
-
-
-
 class Player(pg.sprite.Sprite):
     def __init__(self, map_width, map_height):
-
         super(Player, self).__init__()
 
         self.load_animations()
@@ -69,13 +70,9 @@ class Player(pg.sprite.Sprite):
         self.map_width = map_width
         self.map_height = map_height
 
-
     def update(self):
-
         self.velocity_y += self.gravity
-
         self.rect.y += self.velocity_y
-
 
         if pg.time.get_ticks() - self.timer > self.interval:
             self.current_image += 1
@@ -84,9 +81,6 @@ class Player(pg.sprite.Sprite):
 
             self.image = self.current_animation[self.current_image]
             self.timer = pg.time.get_ticks()
-
-
-
 
     def load_animations(self):
         tile_size = 16
@@ -106,9 +100,6 @@ class Player(pg.sprite.Sprite):
 
     def jump(self):
         self.velocity_y = -TILE_SCALE * 3
-        self.is_jumping = True
-
-
 
 class Game:
     def __init__(self):
@@ -118,7 +109,6 @@ class Game:
         self.record = 0
 
     def setup(self):
-
         self.background = pg.image.load('assets/Background/Background2.png')
         self.background = pg.transform.scale(self.background, (SCREEN_WIDTH, SCREEN_HEIGHT))
         self.clock = pg.time.Clock()
@@ -131,25 +121,23 @@ class Game:
         self.timer = pg.time.get_ticks()
         self.points = 0
         self.score_timer = pg.time.get_ticks()
-        self.menu_box = pg.image.load('assets/HUD Menus Contrast Blue 2.png')
+        menu_box = pg.image.load('assets/menu_box.png')
+        self.menu_box = pg.transform.scale(menu_box, (330, 636))
         self.speed = 5
 
         self.speed_interval = 2500
         self.speed_timer = pg.time.get_ticks()
 
-        self.interval = 2000  # Увеличим интервал между парами труб
-        self.gap_size = 200  # Фиксированный промежуток для пролета
+        self.interval = 2000
+        self.gap_size = 200
 
     def create_pipe_pair(self):
-        # Вычисляем случайную высоту для верхней трубы
         min_height = 50
-        max_height = SCREEN_HEIGHT - self.gap_size - min_height  # Оставляем место для нижней трубы и промежутка
+        max_height = SCREEN_HEIGHT - self.gap_size - min_height
         top_height = random.randint(min_height, max_height)
         
-        # Вычисляем высоту нижней трубы, учитывая промежуток
         bottom_height = SCREEN_HEIGHT - (top_height + self.gap_size)
         
-        # Создаем трубы с заданными высотами
         top_pipe = Pipe(is_top=True, height=top_height)
         bottom_pipe = Pipe(is_top=False, height=bottom_height)
         
@@ -172,73 +160,52 @@ class Game:
             if event.type == pg.MOUSEBUTTONDOWN and event.button == 1 and self.mode == 'game':
                 self.player.jump()
 
-
             if self.mode=='game_over' and event.type == pg.MOUSEBUTTONDOWN:
                 self.setup()
-
 
     def update(self):
         self.player.update()
         if self.mode == 'game':
             self.pipes.update(self.speed)
             if pg.time.get_ticks() - self.timer > self.interval:
-                self.interval -= 10
-
                 self.create_pipe_pair()
                 self.timer = pg.time.get_ticks()
 
-
-
             if pg.time.get_ticks() - self.speed_timer > self.speed_interval:
                 self.speed += 1
+                self.interval = (600 / (self.speed * FPS)) * 1000
                 self.speed_timer = pg.time.get_ticks()
 
-
-
-
-        if pg.sprite.spritecollide(self.player, self.pipes, False) or self.player.rect.y > SCREEN_HEIGHT or self.player.rect.y < 0:
+        if pg.sprite.spritecollide(self.player, self.pipes, False) or self.player.rect.y > SCREEN_HEIGHT:
             self.mode = 'game_over'
-
-
 
         if pg.time.get_ticks() - self.score_timer > 1000:
             if self.mode=='game':
                 self.points += 1
-
-
-
             self.score_timer = pg.time.get_ticks()
-
-
 
         if self.points > self.record:
             self.record = self.points
 
-
-
-
     def draw(self):
         self.screen.blit(self.background, (0,0))
-
         self.all_sprites.draw(self.screen)
         self.pipes.draw(self.screen)
 
         if self.mode == 'game_over':
-            pg.draw.rect(self.screen, pg.Color("Orange"), (SCREEN_WIDTH//2-200, SCREEN_HEIGHT//2-150, 400, 300))
+            menu_rect = self.menu_box.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
+            self.screen.blit(self.menu_box, menu_rect)
             text = font.render(f"Score : {self.points}", True, pg.Color("white"))
-
-            text_rect = text.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2-40))
+            text_rect = text.get_rect(center=(SCREEN_WIDTH//2-60, SCREEN_HEIGHT//2-40))
             self.screen.blit(text, text_rect)
             record_text = font.render(f"Record : {self.record}", True, pg.Color("white"))
-            record_text_rect = text.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2-65))
+            record_text_rect = text.get_rect(center=(SCREEN_WIDTH//2-60, SCREEN_HEIGHT//2-65))
             self.screen.blit(record_text, record_text_rect)
-
             restart_text = font.render('Press LMB to continue', True, pg.Color('white'))
-            restart_text_rect = text.get_rect(center=(SCREEN_WIDTH//2, SCREEN_HEIGHT//2))
+            restart_text_rect = text.get_rect(center=(SCREEN_WIDTH//2-60, SCREEN_HEIGHT//2))
             self.screen.blit(restart_text, restart_text_rect)
         elif self.mode == 'game':
-            self.screen.blit(font.render(f'{self.points}', False, pg.Color('Yellow')), (SCREEN_WIDTH - 50, 20))
-
+            self.screen.blit(score_font.render(f'{self.points}', False, pg.Color('Yellow')), (SCREEN_WIDTH - 50, 20))
 
         pg.display.flip()
 
